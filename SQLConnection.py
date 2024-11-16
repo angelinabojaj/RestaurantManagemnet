2dbb8ca2fa23692a4eda4f1f14360d6eb2060ed5
Angelina Bojaj <156238474+angelinabojaj@users.noreply.github.com>
2024-11-16 00:19:37 -0500

    SQLConnection
    Use this for connecting to database. Follow comments for directions.

---
+++ SQLConnection.py
@@ -0,0 +1,18 @@
+# Preform the following: pip install mysql-connector-python
+import mysql.connector
+
+# This will be subjected to change for everyone
+connection = mysql.connector.connect(
+        host="127.0.0.1", # Subject to change
+        user = "root", # Subject to change
+        password = "334Mommy!!", # Put your root password here
+        database = "RestaurantMangement") # Put your database name here
+    
+mycursor = connection.cursor()
+
+mycursor.execute("SELECT * FROM employee")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+        print(x)
