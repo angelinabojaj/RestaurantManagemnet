68d8c2967fe5a84c504167da79a9a1aa3493c17e
Angelina Bojaj <156238474+angelinabojaj@users.noreply.github.com>
2024-11-17 20:55:26 -0500

    Moving Files To Folder

--- SQLConnection.py
+++ SQLConnection.py
@@ -6,13 +6,65 @@ connection = mysql.connector.connect(
         host="127.0.0.1", # Subject to change
         user = "root", # Subject to change
         password = "334Mommy!!", # Put your root password here
-        database = "rma") # Put your database name here
+        database = "rms") # Put your database name here
 
 mycursor = connection.cursor()
 
-mycursor.execute("SELECT * FROM employee")
+mycursor.execute("SELECT * FROM menu")
 
 myresult = mycursor.fetchall()
 
 for x in myresult:
         print(x)
+
+# Functions For Different Tables
+        # Import this file into the GUI (Which it is already)
+        # Functions can be called by using the file import name followed my a decimal.
+                # Example: SQLConnection.show_menu()
+# Shows menu
+def show_menu():
+        mycursor.execute("SELECT * FROM menu")
+        
+        #  Add code to GUI for display
+        
+        print("Menu Shown")
+
+# Shows notifcications     
+def show_notifications():
+        mycursor.execute("SELECT * FROM notifications")
+        
+        #  Add code to GUI for display
+        
+        print("Notificationa Shown")
+
+# Show orders
+def show_orders():
+        mycursor.execute("SELECT * FROM orders")
+        
+        #  Add code to GUI for display
+        
+        print("Orders Shown")
+
+# Show shift duties
+def show_shiftDuties():
+        mycursor.execute("SELECT * FROM shift_duties")
+        
+        # Add code to GUI for display
+        
+        print("Shift Duties Shown")     
+        
+
+# Show scheduele
+def show_scheduele():
+        mycursor.execute("SELECT * FROM scheduele")
+        
+        #  Add code to GUI for display
+        
+        print("Scheduele Shown")
+
+def show_absent_request():
+        mycursor.execute("SELECT * FROM absent_request")
+        
+        #  Add code to GUI for display
+        
+        print("Absent Request Shown")
