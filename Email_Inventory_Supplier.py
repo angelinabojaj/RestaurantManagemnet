dd539b9acdcb637389e25c2c723e13c561b6bb61
Angelina Bojaj <angelinabojaj@oakland.edu>
2024-11-20 22:27:20 -0500

    Contact Inventory Supplier Working!

--- Email_Inventory_Supplier.py
+++ Email_Inventory_Supplier.py
@@ -1,2 +1,20 @@
+import smtplib
+
 def sendEmail():
+    
+    email = "theanythingconeyisland@gmail.com"
+    emailPassword = "vnow rynt hwot rmaw"
+    receiverEmail = "inventorysupplierou@gmail.com"
+
+    subjectEmail = "Fufillment Needed"
+    messageEmail = "Dear Inventory Supplier,\nThe Anything Conney Island needs a restock done please.\n\nSincerely,\n\nThe Anything Coney Island\ntheanythingconeyisland@gmail.com"
+
+    text = f"Subject: {subjectEmail}\n\n{messageEmail}"
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    
+    server.login(email, emailPassword)
+    
+    server.sendmail(email, receiverEmail, text)
+    
     print("Email Sent")
